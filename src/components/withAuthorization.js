22798dcaa2cb5703bec185c544f5e183bb7cb36a
romeo.hesch <romeo.hesch@gmail.com>
2018-11-13 09:00:27 +0100

    adding authorization to everything

---
+++ src/components/withAuthorization.js
@@ -0,0 +1,34 @@
+import React from 'react'; 
+import {withRouter} from 'react-router-dom';
+import {firebase} from '../firebase';
+import * as routes from '../constants/routes';
+import AuthUserContext from './AuthUserContext';
+
+
+const  withAuthorization = (authCondition) => (Component) => {
+  class WithAuthorization extends React.Component {
+
+    componentDidMount(){
+      firebase.auth.onAuthStateChanged(authUser =>{
+        if(!authCondition(authUser)) {
+          this.props.history.push(routes.SIGNIN)
+        }
+      });
+    }
+
+    render(){
+      return (
+        <AuthUserContext.Consumer>
+        {
+          authUser => authUser 
+          ? <Component {...this.props} />
+          : null
+        }
+        </AuthUserContext.Consumer>
+      );
+    }
+  }
+  return withRouter(WithAuthorization);
+}
+
+export default withAuthorization;
