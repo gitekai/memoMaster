97d2b88d9cc5b1dfdd9cdab6781cb849580c1dfd
romeo.hesch <romeo.hesch@gmail.com>
2018-11-14 17:16:06 +0100

    inputs with controlled focus and snackbar added to mayorsystem

--- src/components/MajorSystemInput.js
+++ src/components/MajorSystemInput.js
@@ -1,117 +1,164 @@
 import React from "react";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
-import AuthUserContext from './AuthUserContext';
-import withAuthorization from './withAuthorization';
-import withMajorNumbers from './withMajorNumber';
-import { database } from '../firebase';
-import './MajorSystem.css';
+import AuthUserContext from "./AuthUserContext";
+import withAuthorization from "./withAuthorization";
+import withMajorNumbers from "./withMajorNumber";
+import { database } from "../firebase";
+import ReactDOM from "react-dom";
+import Snackbar from "@material-ui/core/Snackbar";
 
+import "./MajorSystem.css";
 
-
-const initialState = (majorNumberLength) => {
+const initialState = majorNumberLength => {
   const retObj = {};
-  const startPos = '0'.repeat(majorNumberLength);
-  for (let i = startPos; i.length === majorNumberLength; i = returnNextCount(i)) {
-    retObj[i]='';
+  const startPos = "0".repeat(majorNumberLength);
+  for (
+    let i = startPos;
+    i.length === majorNumberLength;
+    i = returnNextCount(i)
+  ) {
+    retObj[i] = "";
   }
-  return {...retObj}; 
-}
+  return { ...retObj };
+};
 
+const initialMajorSize = 2;
 class MajorSystemInputs extends React.Component {
-
-
   state = {
-    inputs: initialState(2),
-    majorNumberLength:2,
-    actualFocus: 0,
+    inputs: initialState(initialMajorSize),
+    majorNumberLength: initialMajorSize,
+    actualFocusItem: null,
+    focusRef: {},
+    snackbarOpen: false
   };
 
-  onSubmit = (authUser) => {
-    const { inputs } = this.state;
-    database.doCreateMayorSystem(authUser.uid, inputs);
+  onSnackbarClose = () => {
+    this.setState({snackbarOpen:false})
   }
 
-  componentWillReceiveProps(nextProps){
-    this.setState({ inputs: nextProps.majorSystem})
-  }
 
-  onRangeChange = (value) => {
+  onFocus = newIndex => {
     this.setState({
-      majorNumberLength: 1*value
+      actualFocusItem: newIndex
     });
+  };
+
+  componentDidUpdate() {
+    const actualFocus = this.state.actualFocusItem;
+    if (!actualFocus) {
+      return;
+    }
+    this.state.focusRef[this.state.actualFocusItem].focus();
   }
 
+  onSubmit = authUser => {
+    const { inputs } = this.state;
+    database.doCreateMayorSystem(authUser.uid, inputs)
+    .then(noseque => { this.setState({snackbarOpen: true})});
+  };
+
+  componentWillReceiveProps(nextProps) {
+    if (!nextProps.majorSystem) {
+      return;
+    }
+
+    this.setState({ inputs: nextProps.majorSystem });
+  }
+  onRangeChange = value => {
+    this.setState({
+      majorNumberLength: 1 * value
+    });
+  };
 
   onInputChange = (event, displayNumber) => {
     const value = event.target.value;
+    const focusRef = this.state.focusRef;
+
+    const activeKey = Object.keys(focusRef).filter(
+      ref => document.activeElement === ReactDOM.findDOMNode(focusRef[ref])
+    );
+
     this.setState(currVal => {
-      const kk = (/\s+$/.test(value)) ? currVal.actualFocus + 1 : currVal.actualFocus;
-      return { inputs: { ...currVal.inputs, [displayNumber]: value }, actualFocus: kk };
+      const focusItem = /\s+$/.test(value)
+        ? returnNextCount(activeKey)
+        : activeKey;
+      return {
+        inputs: { ...currVal.inputs, [displayNumber]: value },
+        actualFocusItem: focusItem
+      };
+    });
+  };
+
+  setFocus = (ref, obj) => {
+    this.setState(currState => {
+      const refObj = currState.focusRef;
+      return { focusRef: { ...refObj, [obj]: ref } };
     });
   };
 
   getInputArray() {
-    const { majorNumberLength, inputs, actualFocus } = this.state;
+    const { majorNumberLength, inputs } = this.state;
 
     const elementArray = [];
-    const startPos = '0'.repeat(majorNumberLength);
-    for (let i = startPos,count=0; i.length === majorNumberLength; i = returnNextCount(i),count++) {
-      const className = ( (count + 1) % 10 === 0 ) ? 'extraSpace' : ''
-      //const ref = (count===3) ? (ref) => { this.focused = ref} : null
-      //<Input pos={i} value={inputs[i]} onChange={event => this.onInputChange(event, i)} cl={className} r={ref}/>
-      const element = 
-      <div className={`majorInputItem ${className}`}>
-      {count === actualFocus 
-    ? <TextField
-      className={className}
-      autoFocus
-      variant="outlined"
-      fullWidth
-      type="text"
-      label={i}
-      key={i}
-      onChange={event => this.onInputChange(event, i)}
-      value={inputs[i]}
-    /> 
-    : <TextField
-    className={className}
-    variant="outlined"
-    fullWidth
-    type="text"
-    label={i}
-    key={i}
-    onChange={event => this.onInputChange(event, i)}
-    value={inputs[i]}
-    />}
-    </div>
-    
-    elementArray.push(element);
+    const startPos = "0".repeat(majorNumberLength);
+    for (
+      let i = startPos, count = 0;
+      i.length === majorNumberLength;
+      i = returnNextCount(i), count++
+    ) {
+      const className = (count + 1) % 10 === 0 ? "extraSpace" : "";
+      const element = (
+        <div className={`majorInputItem ${className}`}>
+          <TextField
+            fullWidth
+            className={className}
+            inputRef={ref => {
+              this.setFocus(ref, i);
+            }}
+            variant="outlined"
+            type="text"
+            label={i}
+            key={i}
+            onChange={event => this.onInputChange(event, i)}
+            value={inputs[i]}
+          />
+        </div>
+      );
+
+      elementArray.push(element);
     }
 
     return elementArray;
   }
 
   render() {
-    const { majorNumberLength } = this.state;
+    const { majorNumberLength, snackbarOpen } = this.state;
     return (
       <div id="majorSystemInput">
-        <LengthSelect value={majorNumberLength} handleChange={(event) => this.onRangeChange(event.target.value)} />
+        <LengthSelect
+          value={majorNumberLength}
+          handleChange={event => this.onRangeChange(event.target.value)}
+        />
         <div id="inputs">{this.getInputArray()}</div>
+        <SnackAlert openVal={snackbarOpen} closeHandler={this.onSnackbarClose} />
         <AuthUserContext.Consumer>
-          {
-            authUser =>
-              <Button variant="contained" color="primary" onClick={() => { this.onSubmit(authUser) }}>
-                Submit that shit
-              </Button>
-          }
+          {authUser => (
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => {
+                this.onSubmit(authUser);
+              }}
+            >
+              Submit that shit
+            </Button>
+          )}
         </AuthUserContext.Consumer>
       </div>
     );
   }
 }
-
-
 
 function returnNextCount(countActual) {
   const strCountActual = "" + countActual;
@@ -133,42 +180,57 @@ function returnNextCount(countActual) {
   }
 }
 
-
 const Input = ({ pos, value, onChange, cl, r }) => {
   return (
     <div className={`majorInputItem ${cl}`}>
-    <TextField
-      className={cl}
-      ref={r}
-      variant="outlined"
-      fullWidth
-      type="text"
-      label={pos}
-      key={pos}
-      onChange={onChange}
-      value={value}
-    />
+      <TextField
+        className={cl}
+        ref={r}
+        variant="outlined"
+        fullWidth
+        type="text"
+        label={pos}
+        key={pos}
+        onChange={onChange}
+        value={value}
+      />
     </div>
   );
-}
+};
 
-const LengthSelect = ({ value, handleChange }) =>
-<div>
-<input
-type="range"
-label="range"
-min="1"
-max="2"
-step="1"
-value={value}
-onChange={handleChange}
+const LengthSelect = ({ value, handleChange }) => (
+  <div>
+    <input
+      type="range"
+      label="range"
+      min="1"
+      max="2"
+      step="1"
+      value={value}
+      onChange={handleChange}
+    />
+    <datalist id="tickmarks">
+      <option value="1" label="1" />
+      <option value="2" label="2" />
+    </datalist>
+  </div>
+);
+
+const SnackAlert = ({openVal,closeHandler}) => 
+<Snackbar
+anchorOrigin={{
+  vertical: "bottom",
+  horizontal: "left"
+}}
+open={openVal}
+onClose={closeHandler}
+autoHideDuration={6000}
+ContentProps={{
+  "aria-describedby": "message-id"
+}}
+message={<span id="message-id">MajorSystem Saved succesfully</span>}
 />
-<datalist id="tickmarks">
-  <option value="1" label="1" />
-  <option value="2" label="2" />
-</datalist>
-</div>
 
 const majorNumberInputElement = withMajorNumbers(MajorSystemInputs);
-const authCondition = (authUser) => !!authUser;
+const authCondition = authUser => !!authUser;
 export default withAuthorization(authCondition)(majorNumberInputElement);
